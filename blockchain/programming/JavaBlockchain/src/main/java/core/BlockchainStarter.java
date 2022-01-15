5acbe5d0cbce10f7392cdd917e4ca02de2b383ad
KimH4nKyul <alskim93@kakao.com>
2022-01-15 17:30:11 +0900

    merge this

--- blockchain/programming/JavaBlockchain/src/main/java/core/BlockchainStarter.java
+++ blockchain/programming/JavaBlockchain/src/main/java/core/BlockchainStarter.java
@@ -10,7 +10,7 @@ public class BlockchainStarter {
         int nonce = 0; // 채굴 난이도, 2의 24제곱이라면 24번 중에 한 번만 채굴에 성공할 수 있다는 것
         // 채굴
         while(true) {
-            if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
+            if(Util.getHash(nonce + "").startsWith("000000")) {
                 System.out.println("채굴 성공: " + nonce);
                 System.out.println("해시 값: " + Util.getHash(nonce + ""));
 
